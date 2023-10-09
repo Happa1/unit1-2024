130ddeefff2f8cf293bbc596cfc93ace91a7bb9c
Happa1 <142579414+Happa1@users.noreply.github.com>
2023-10-09 15:32:48 +0900

    Create quiz014.py

---
+++ quizzes/quiz014.py
@@ -0,0 +1,16 @@
+# Quiz014
+
+Fig. 1 prompt of quiz 014
+
+## 1.flow of chart
+
+Fig. 2 algorithm flow chart of quiz 014
+
+## 2.solution
+```.py
+
+```
+
+## 3.proof of work
+
+Fig. 3 test of quiz 012
